a7012b9c9f03f091dc84b10907cea36531aa8d90
Dan <dan.arias@auth0.com>
2020-07-01 10:16:35 -0400

    Call an API

--- src/auth0-provider-with-history.js
+++ src/auth0-provider-with-history.js
@@ -6,6 +6,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
   const history = useHistory();
   const domain = process.env.REACT_APP_AUTH0_DOMAIN;
   const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const audience = process.env.REACT_APP_AUDIENCE;
 
   const onRedirectCallback = (appState) => {
     history.push(appState?.returnTo || window.location.pathname);
@@ -17,6 +18,7 @@ const Auth0ProviderWithHistory = ({ children }) => {
       clientId={clientId}
       redirectUri={window.location.origin}
       onRedirectCallback={onRedirectCallback}
+      audience={audience}
     >
       {children}
     </Auth0Provider>
--- src/views/external-api.js
+++ src/views/external-api.js
@@ -1,9 +1,45 @@
-import React from "react";
-import { Button, Container } from "react-bootstrap";
-import { withAuthenticationRequired } from "@auth0/auth0-react";
-import { Loading } from "../components";
-
+import React, { useState } from "react";
+import { Button, ButtonGroup, Container } from "react-bootstrap";
+import { withAuthenticationRequired, useAuth0 } from "@auth0/auth0-react";
+import { Loading, Highlight } from "../components";
 export const ExternalApi = () => {
+  const [message, setMessage] = useState("");
+
+  const { getAccessTokenSilently } = useAuth0();
+
+  const callAPI = async () => {
+    try {
+      const response = await fetch("http://localhost:7000/api/public-message");
+
+      const responseData = await response.json();
+
+      setMessage(responseData);
+    } catch (error) {
+      setMessage(error.message);
+    }
+  };
+
+  const callSecureApi = async () => {
+    try {
+      const token = await getAccessTokenSilently();
+
+      const response = await fetch(
+        `http://localhost:7000/api/private-message`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const responseData = await response.json();
+
+      setMessage(responseData);
+    } catch (error) {
+      setMessage(error.message);
+    }
+  };
+
   return (
     <Container className="mb-5">
       <h1>External API</h1>
@@ -12,10 +48,23 @@ export const ExternalApi = () => {
         and the API will validate it using the API's audience value.{" "}
         <strong>This route should be private</strong>.
       </p>
+      <ButtonGroup>
+        <Button onClick={callAPI} color="primary" className="mt-5">
+          Get Public Message
+        </Button>
+        <Button onClick={callSecureApi} color="primary" className="mt-5">
+          Get Private Message
+        </Button>
+      </ButtonGroup>
 
-      <Button color="primary" className="mt-5">
-        Ping API
-      </Button>
+      {message && (
+        <div className="mt-5">
+          <h6 className="muted">Result</h6>
+          <Highlight language="json">
+            {JSON.stringify(message, null, 2)}
+          </Highlight>
+        </div>
+      )}
     </Container>
   );
 };
