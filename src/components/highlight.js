175544a763743e0afe13c004572c736e5a0582dc
Dan <dan.arias@auth0.com>
2020-07-01 10:19:51 -0400

    Update Highlight component and API server

--- src/components/highlight.js
+++ src/components/highlight.js
@@ -1,63 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
 
-import hljs from "highlight.js";
-import "highlight.js/styles/monokai-sublime.css";
+import { a11yDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import SyntaxHighlighter from "react-syntax-highlighter";
 
-const registeredLanguages = {};
-
-class Highlight extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { loaded: false };
-    this.codeNode = React.createRef();
-  }
-
-  componentDidMount() {
-    const { language } = this.props;
-
-    if (language && !registeredLanguages[language]) {
-      try {
-        const newLanguage = require(`highlight.js/lib/languages/${language}`);
-        hljs.registerLanguage(language, newLanguage);
-        registeredLanguages[language] = true;
-
-        this.setState({ loaded: true }, this.highlight);
-      } catch (e) {
-        console.error(e);
-        throw Error(`Cannot register the language ${language}`);
-      }
-    } else {
-      this.setState({ loaded: true });
-    }
-  }
-
-  componentDidUpdate() {
-    this.highlight();
-  }
-
-  highlight = () => {
-    this.codeNode &&
-      this.codeNode.current &&
-      hljs.highlightBlock(this.codeNode.current);
-  };
-
-  render() {
-    const { language, children } = this.props;
-    const { loaded } = this.state;
-
-    if (!loaded) {
-      return null;
-    }
-
-    return (
-      <pre className="rounded">
-        <code ref={this.codeNode} className={language}>
-          {children}
-        </code>
-      </pre>
-    );
-  }
-}
+const Highlight = ({ children, language }) => (
+  <SyntaxHighlighter language={language} style={a11yDark}>
+    {children}
+  </SyntaxHighlighter>
+);
 
 export default Highlight;
